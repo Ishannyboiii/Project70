1f52727647bc267b1bf8f4039281b6f519c5b630
Ishaan Bhardwaj <83829098+Ishannyboiii@users.noreply.github.com>
2021-12-27 18:34:27 -0800

    Add files via upload

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import {createButtonTabNavigator} from "@react-navigation/bottom-tabs";
+import TransactionScreen from '../screens/transaction';
+import SearchScreen from '../screens/search';
+
+const Tab = createButtonTabNavigator();
+
+export default class BottomTabNavigator extends Component {
+    render()
+    {
+        return(
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen>
+                        name = "Transaction" component = {TransactionScreen}
+                    </Tab.Screen>
+                    <Tab.Screen>
+                        name = "Search" component = {SearchScreen}
+                    </Tab.Screen>
+                </Tab.Navigator>
+            </NavigationContainer>
+        )
+    }
+}
+
---
+++ screens/transaction.js
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+export default class TransactionScreen extends Component {
+    render()
+    {
+        return(
+        <View style = {styles.container}>
+         <Text style = {styles.text}>
+             Transaction Screen
+         </Text>
+        </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text:{
+        color: '#ffff',
+        fontSize: 30,
+    }
+  });
